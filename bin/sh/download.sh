16a9ad84b7882f82c82abbb0fa6f4029e069b42d
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2021-07-05 17:35:10 -0500

    Improved logging

--- bin/sh/download.sh
+++ bin/sh/download.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 log(){
-  printf "[%s]: %s\n" "$0" "$1"
+  printf "[%s] %s\n" "$0" "$1"
 }
 
+GET_MP3="--get-mp3"
 get_mp3(){
-  log "url: $1"
+  log "$GET_MP3 (url: $1) =>"
   youtube-dl -o "%(title)s.%(ext)s" \
 	--ignore-errors \
 	--extract-audio \
@@ -13,8 +14,9 @@ get_mp3(){
 	mp3 "$1"
 }
 
+GET_MP3S="--get-mp3-playlist"
 get_mp3_playlist(){
-  log "url: $1"
+  log "$GET_MP3S (url: $1) =>"
 	youtube-dl -o "%(playlist_index)s-%(title)s.%(ext)s" \
 	--ignore-errors \
 	--extract-audio \
@@ -24,5 +26,5 @@ get_mp3_playlist(){
 
 echo "$#"
 
-[ "--get-mp3" == "$1" ] && [ "$#" == "2" ] && get_mp3 "$2"
-[ "--get-mp3-playlist" == "$1" ] && [ "$#" == "2" ] && get_mp3_playlist "$2"
+[ "$GET_MP3" == "$1" ] && [ "$#" == "2" ] && get_mp3 "$2"
+[ "$GET_MP3S" == "$1" ] && [ "$#" == "2" ] && get_mp3_playlist "$2"
